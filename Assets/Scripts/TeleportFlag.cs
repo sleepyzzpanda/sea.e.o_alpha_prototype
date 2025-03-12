27a8f49ace4c854515af7e593f3bbdb9163b0e14
Qiraa Qadri <107440444+sleepyzzpanda@users.noreply.github.com>
2025-03-12 02:56:46 -0700

    tested teleporting

---
+++ Assets/Scripts/TeleportFlag.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportFlag : MonoBehaviour
+{
+    public GameObject leadsTo;
+    public Vector3 cameraPos, newPlayerPos;
+   
+}
---
+++ Assets/Scripts/TeleportManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour
+{
+    public GameObject player, camera;
+    public LayerMask teleportLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player overlaps with teleport flag item, teleport player to the location specified by the flag
+        // flags are on TELEPORT layer
+        if(Physics2D.OverlapCircle(player.transform.position, 0.15f, teleportLayer)){
+            TeleportFlag flag = Physics2D.OverlapCircle(player.transform.position, 0.15f, teleportLayer).GetComponent<TeleportFlag>();
+            player.transform.position = flag.newPlayerPos; // set player pos
+            // player move point too
+            player.GetComponent<player_behavior>().move_point.position = flag.newPlayerPos;
+            camera.transform.position = flag.cameraPos;
+            
+        }
+
+
+        
+    }
+}
